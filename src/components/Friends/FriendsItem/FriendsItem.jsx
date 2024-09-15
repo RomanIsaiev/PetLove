773dd730487ef4920089764e7b2b0abf9f0d1bbd
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-09-16 01:37:01 +0300

    friends page styled

--- src/components/Friends/FriendsItem/FriendsItem.jsx
+++ src/components/Friends/FriendsItem/FriendsItem.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import cl from "./FriendsItem.module.scss";
 
 export const FriendsItem = ({ data }) => {
   const { address, addressUrl, email, imageUrl, phone, title, url, workDays } =
@@ -8,64 +9,62 @@ export const FriendsItem = ({ data }) => {
   const weekDay = date.getDay();
 
   return (
-    <li>
+    <li className={cl.item}>
       <div>
-        <div>
-          <img src={imageUrl} alt={title} width="90" height="90" />
-        </div>
-        <div>
-          <a href={url} target="_blank" rel="nofollow">
-            <h3>{title}</h3>
-          </a>
-          <ul>
-            <li>
-              {email ? (
-                <a href={`mailto:${email}`} target="_blank" rel="nofollow">
-                  <span>Email:</span> {email}
-                </a>
-              ) : (
-                <p>
-                  <span>Email:</span> website only
-                </p>
-              )}
-            </li>
-            <li>
-              {address ? (
-                <a href={addressUrl} target="_blank" rel="nofollow">
-                  <span>Address:</span> {address}
-                </a>
-              ) : (
-                <p>
-                  <span>Address:</span> website only
-                </p>
-              )}
-            </li>
-            <li>
-              {phone ? (
-                <a href={`tel:${phone}`} target="_blank" rel="nofollow">
-                  <span>Phone:</span> {phone ?? "email only"}
-                </a>
-              ) : (
-                <p>
-                  <span>Phone:</span> website only
-                </p>
-              )}
-            </li>
-          </ul>
-        </div>
-        <div>
-          {workDays ? (
-            workDays?.[weekDay]?.isOpen ? (
-              <span>
-                {workDays?.[weekDay]?.from} - {workDays?.[weekDay]?.to}{" "}
-              </span>
+        <img src={imageUrl} alt={title} width="90" height="90" />
+      </div>
+      <div>
+        <a href={url} target="_blank" rel="nofollow">
+          <h3 className={cl.title}>{title}</h3>
+        </a>
+        <ul className={cl.contactsList}>
+          <li className={cl.contactsItem}>
+            {email ? (
+              <a href={`mailto:${email}`} target="_blank" rel="nofollow">
+                <span>Email:</span> {email}
+              </a>
             ) : (
-              "Closed"
-            )
+              <p>
+                <span>Email:</span> website only
+              </p>
+            )}
+          </li>
+          <li className={cl.contactsItem}>
+            {address ? (
+              <a href={addressUrl} target="_blank" rel="nofollow">
+                <span>Address:</span> {address.split(" ").slice(0, 3).join(" ")}
+              </a>
+            ) : (
+              <p>
+                <span>Address:</span> website only
+              </p>
+            )}
+          </li>
+          <li className={cl.contactsItem}>
+            {phone ? (
+              <a href={`tel:${phone}`} target="_blank" rel="nofollow">
+                <span>Phone:</span> {phone ?? "email only"}
+              </a>
+            ) : (
+              <p>
+                <span>Phone:</span> website only
+              </p>
+            )}
+          </li>
+        </ul>
+      </div>
+      <div className={cl.workTimeContainer}>
+        {workDays ? (
+          workDays?.[weekDay]?.isOpen ? (
+            <span className={cl.workTime}>
+              {workDays?.[weekDay]?.from} - {workDays?.[weekDay]?.to}
+            </span>
           ) : (
-            "Day and night"
-          )}
-        </div>
+            <span className={cl.workTime}>Closed</span>
+          )
+        ) : (
+          <span className={cl.workTime}>Day and night</span>
+        )}
       </div>
     </li>
   );
--- src/components/Friends/FriendsList/FriendsList.jsx
+++ src/components/Friends/FriendsList/FriendsList.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import { FriendsItem } from "../FriendsItem/FriendsItem";
+import cl from "./FriendsList.module.scss";
 
 export const FriendsList = ({ friends }) => {
   console.log(friends);
   return (
-    <ul>
+    <ul className={cl.list}>
       {friends.map((item) => (
         <FriendsItem key={item._id} data={item} />
       ))}
